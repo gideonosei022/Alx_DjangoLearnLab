add733086fac6148aad72626d6155c192eb93855
Gideon Osei <gideonosei022@gmail.com>
2026-01-23 03:57:02 +0000

    custom

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -12,8 +12,9 @@ INSTALLED_APPS = [
 LOGIN_REDIRECT_URL = "/books/"
 LOGOUT_REDIRECT_URL = "/login/"
 LOGIN_URL = "/login/"
-
+# Use the custom user model
 AUTH_USER_MODEL = "bookshelf.CustomUser"
+
 
 
 MEDIA_URL = "/media/"
