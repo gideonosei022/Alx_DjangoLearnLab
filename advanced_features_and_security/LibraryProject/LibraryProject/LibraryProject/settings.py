717603baa6b80e2e04ab1bab82b2e1ea939672f6
Gideon Osei <gideonosei022@gmail.com>
2026-01-23 04:46:39 +0000

    custom

--- advanced_features_and_security/LibraryProject/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/LibraryProject/settings.py
@@ -1,2 +1,23 @@
 # Tell Django to use the custom user model
 AUTH_USER_MODEL = "bookshelf.CustomUser"
+
+
+# SECURITY SETTINGS
+
+# Prevent cross-site scripting (XSS) attacks in supported browsers
+SECURE_BROWSER_XSS_FILTER = True
+
+# Prevent your site from being rendered in a frame (clickjacking protection)
+X_FRAME_OPTIONS = 'DENY'
+
+# Prevent the browser from guessing content types incorrectly
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Ensure CSRF cookie is only sent over HTTPS
+CSRF_COOKIE_SECURE = True
+
+# Ensure session cookie is only sent over HTTPS
+SESSION_COOKIE_SECURE = True
+
+# Set DEBUG=False in production (already recommended)
+DEBUG = False
