8be28ba1d33dfd9ead3cd0378717ea9321adedf4
Gideon Osei <gideonosei022@gmail.com>
2026-01-23 04:37:52 +0000

    nice

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -52,4 +52,27 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+# ---------------------------
+# Book Model with Permissions
+# ---------------------------
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
 
